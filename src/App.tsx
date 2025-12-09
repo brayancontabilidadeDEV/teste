08a47e901d3867ec3995a260e52190db6280b496
brayancontabilidadeDEV <brayancontabilidade@gmail.com>
2025-12-09 11:17:59 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import TaxCalculator from '@/components/TaxCalculator';
+import { Toaster } from '@/components/ui/sonner';
+
+function App() {
+  return (
+    <>
+      <TaxCalculator />
+      <Toaster />
+    </>
+  );
+}
+
+export default App; 
